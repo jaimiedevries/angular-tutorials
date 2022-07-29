d3f7a0dcd4fe1c2e23cb2cbdce4bd28e58184846
Jaimie de Vries <49305844+jaimiedevries@users.noreply.github.com>
2022-07-29 11:30:06 +0200

    Lijst met heroes toegevoegd en wat css wijzigingen

--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  {id: 12, name: 'Spiderman'},
-  {id: 13, name: 'Wolverine'},
-  {id: 14, name: 'Gambit'},
-  {id: 15, name: 'Deadpool'},
-  {id: 16, name: 'Iron Man'},
-  {id: 17, name: 'Thor'},
-  {id: 18, name: 'Hulk'},
-  {id: 19, name: 'Black Panther'},
-  {id: 20, name: 'Captain America'}
+  {id: 12, name: 'Spiderman', emoji: '🕷️'},
+  {id: 13, name: 'Wolverine', emoji: '🐺'},
+  {id: 14, name: 'Gambit', emoji: '🎲'},
+  {id: 15, name: 'Deadpool', emoji: '💀'},
+  {id: 16, name: 'Iron Man', emoji: '🤖'},
+  {id: 17, name: 'Thor', emoji: '⚡'},
+  {id: 18, name: 'Hulk', emoji: '💪'},
+  {id: 19, name: 'Black Panther', emoji: '🐯'},
+  {id: 20, name: 'Captain America', emoji: '🇺🇸'}
 ];
